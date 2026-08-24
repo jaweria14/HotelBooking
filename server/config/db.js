28dc1298ff84e1d693b41838a0854681f7c36c87
Jaweria Imtiaz <jaweriaimtiaz01@gmail.com>
2026-08-25 00:27:33 +0500

    Debug MongoDB connection

--- server/config/db.js
+++ server/config/db.js
@@ -6,6 +6,7 @@ dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDb= async()=>{
    try{
+      console.log("MongoDB URI exists:", !!process.env.MONGODB_URI);
     mongoose.connection.on('connected' ,()=>{
         console.log('mongodb connected')
     })
