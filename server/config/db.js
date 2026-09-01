6d5e10331635217a65f18ad8b1c87c6d57dc93a2
Jaweria Imtiaz <jaweriaimtiaz01@gmail.com>
2026-09-01 15:30:20 +0500

    mongo db connection check

--- server/config/db.js
+++ server/config/db.js
@@ -1,20 +1,19 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import dns from "dns";
 
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
+const connectDb = async () => {
+    try {
+        console.log("MongoDB URI exists:", !!process.env.MONGODB_URI);
 
-const connectDb= async()=>{
-   try{
-      console.log("MongoDB URI exists:", !!process.env.MONGODB_URI);
-    mongoose.connection.on('connected' ,()=>{
-        console.log('mongodb connected')
-    })
-     await mongoose.connect(`${process.env.MONGODB_URI}`)
-   }
-   catch(error){
-    console.log(error.message)
-   }
-}
+        await mongoose.connect(process.env.MONGODB_URI);
+
+        console.log("✅ mongodb connected");
+
+    } catch (error) {
+        console.log("❌ MongoDB connection error:", error.message);
+    }
+};
 
 export default connectDb;
