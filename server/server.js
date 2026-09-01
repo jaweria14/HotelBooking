01eef900ff2695bb985ade32663156488bb8fc10
Jaweria Imtiaz <jaweriaimtiaz01@gmail.com>
2026-09-01 14:49:34 +0500

    fix the file name

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import hotelRouter from "./routes/hotelRoutes.js";
 import connectCloudinary from "./config/cloudinary.js";
 import roomRouter from "./routes/roomRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
-import { stripeWebhooks } from "./controller/stripewebhooks.js";
+import {stripeWebhooks} from "./controller/stripeWebhooks.js";
 
 connectDb() ;
 connectCloudinary();
